b03e3d5aa1859ad9a46af3eabace3cb2eaddc87c
Unknown <louis.benjamin@live.fr>
2017-10-19 10:26:37 +0200

    layout V2 addbutton intent

--- app/src/main/java/com/example/benjaminlouis/projetfinal/Main.java
+++ app/src/main/java/com/example/benjaminlouis/projetfinal/Main.java
@@ -1,5 +1,6 @@
 package com.example.benjaminlouis.projetfinal;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -27,8 +28,8 @@ public class Main extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(Main.this,AjoutActivity.class);
+                startActivity(intent);
             }
         });
 
